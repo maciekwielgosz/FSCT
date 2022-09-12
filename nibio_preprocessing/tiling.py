fa59bdd3be3e64230c90b2ec9fd0b65cc6380203
Maciej Wielgosz <maciej.wielgosz@nibio.no>
2022-09-12 16:21:59 +0200

    tile index

--- nibio_preprocessing/tiling.py
+++ nibio_preprocessing/tiling.py
@@ -2,6 +2,7 @@ import argparse
 import pdal
 import os, glob, shutil
 import json
+import pandas as pd
 from tqdm import tqdm
 
 from pathlib import Path
@@ -11,13 +12,14 @@ class Tiling:
     """
     The tiling operation on las files is done by the pdal splitter filter.
     """
-    def __init__(self, input_folder, output_folder, tile_size=10, tile_buffer=0, do_mapping_to_ply=False):
+    def __init__(self, input_folder, output_folder, tile_size=10, tile_buffer=0, do_mapping_to_ply=False, do_tile_index=False):
         self.input_folder = input_folder
         self.output_folder = output_folder
         self.output_folder_ply = output_folder + "_ply"
         self.tile_size = tile_size
         self.tile_buffer = tile_buffer
         self.do_mapping_to_ply = do_mapping_to_ply
+        self.do_tile_index = do_tile_index
  
 
     def do_tiling_of_single_file(self, file):
@@ -61,6 +63,10 @@ class Tiling:
         # convert the tiles to ply
         if self.do_mapping_to_ply:
             self.convert_files_in_folder_from_las_to_ply(file_folder)
+
+        # get the tile index
+        if self.do_tile_index:
+            self.get_tile_index(file_folder)
         
 
     def do_tiling_of_files_in_folder(self):
@@ -117,17 +123,36 @@ class Tiling:
         for file in tqdm(files):
             self.convert_single_file_from_las_to_ply(file)
 
-    def get_tile_index(self):
-        pass
+    def get_tile_index(self, folder=None):
+        """
+        This function will create a tile index for all the tiles in a folder
+        """
+        tile_index = pd.DataFrame(columns=['tile', 'x', 'y'])
+        files = glob.glob(os.path.join(folder, "*.ply"))
+
+        for i, ply in tqdm(enumerate(files), total=len(files)):
+            T = int(os.path.split(ply)[1].split('.')[0])
+            reader = {"type":f"readers{os.path.splitext(ply)[1]}", "filename":ply}
+            stats =  {"type":"filters.stats", "dimensions":"X,Y"}
+            JSON = json.dumps([reader, stats])
+            pipeline = pdal.Pipeline(JSON)
+            pipeline.execute()
+            JSON = json.loads(pipeline.metadata)
+            X = JSON['metadata']['filters.stats']['statistic'][0]['average']
+            Y = JSON['metadata']['filters.stats']['statistic'][1]['average']
+            tile_index.loc[i, :] = [T, X, Y]   
+
+        tile_index.to_csv(os.path.join(folder, 'tile_index.dat'), index=False, header=False, sep=' ')
+
 
     def run(self):
         self.do_tiling_of_files_in_folder()
 
-def main(input_folder, output_folder, tile_size=10, tile_buffer=0, do_mapping_to_ply=False):
+def main(input_folder, output_folder, tile_size=10, tile_buffer=0, do_mapping_to_ply=False, do_tile_index=False):
     """
     This function will tile all the files in a folder
     """
-    tiling = Tiling(input_folder, output_folder, tile_size, tile_buffer, do_mapping_to_ply)
+    tiling = Tiling(input_folder, output_folder, tile_size, tile_buffer, do_mapping_to_ply, do_tile_index)
     tiling.run()
 
 
@@ -172,10 +197,25 @@ if __name__ == "__main__":
         required=False,
         default=True,
     )
+    parser.add_argument(
+        "-g",
+        "--do_tile_index",
+        type=bool,
+        help="Get tile index",
+        required=False,
+        default=True,
+    )
 
     args = parser.parse_args()
 
-    main(args.input_folder, args.output_folder, args.tile_size, args.tile_buffer, args.do_mapping_to_ply)
+    main(
+        args.input_folder, 
+        args.output_folder, 
+        args.tile_size, 
+        args.tile_buffer, 
+        args.do_mapping_to_ply,
+        args.do_tile_index
+        )
 
 
 

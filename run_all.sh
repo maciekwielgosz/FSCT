62141ea8d0d499e83597de8778f99ae6fd903824
Maciej Wielgosz <maciej.wielgosz@nibio.no>
2022-10-06 15:37:22 +0200

    update of the script for running a whole pipeline

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-############################ paramters #################################################
+############################ parameters #################################################
 # General parameters
 CLEAR_INPUT_FOLDER=1  # 1: clear input folder, 0: not clear input folder
+CONDA_ENV="pdal-env-1" # conda environment for running the pipeline
 
-# TODO: Add these parameters the file at the end !!!
 # Tiling parameters
 N_TILES=3
 SLICE_THICKNESS=0.5
@@ -17,8 +17,24 @@ FIND_STEMS_MIN_POINTS=50
 
 
 # Do the environment setup
-export PYTHONPATH='.'
-conda activate pdal-env-1
+# check if PYTHONPATH is set to the current directory
+if [ -z "$PYTHONPATH" ]; then
+    echo "PYTHONPATH is not set. Setting it to the current directory"
+    export PYTHONPATH=$PWD
+else
+    echo "PYTHONPATH is set to '$PYTHONPATH'"
+fi
+
+# conda activate pdal-env-1
+
+# check if conda is activated if not activate it
+if [ -z "$CONDA_ENV" ]; then
+    echo "Conda is not activated. Activating it"
+    conda activate pdal-env-1
+else
+    echo "Conda is activated"
+fi
+
 
 # read input folder as a command line argument
 # show a message to provide the input folder
@@ -31,36 +47,33 @@ then
     exit 1
 fi
 
-# # clear input folder if CLEAR_INPUT_FOLDER is set to 1
-# if [ $CLEAR_INPUT_FOLDER -eq 1 ]
-# then
-#     # delete all the files and folders except the ply files in the input folder
-#     echo "Clearing input folder"
-#     find $data_folder/ -type f ! -name '*.ply' -delete # delete all the files except the ply files
-#     find $data_folder/* -type d -exec rm -rf {} + # delete all the folders in the input folder
-# fi
+# clear input folder if CLEAR_INPUT_FOLDER is set to 1
+if [ $CLEAR_INPUT_FOLDER -eq 1 ]
+then
+    # delete all the files and folders except the ply files in the input folder
+    echo "Clearing input folder"
+    find $data_folder/ -type f ! -name '*.ply' -delete # delete all the files except the ply files
+    find $data_folder/* -type d -exec rm -rf {} + # delete all the folders in the input folder
+fi
 
-# # # iterate over all files in the input folder and do sematic segmentation
-# echo  "Starting semantic segmentation"
-# for file in $data_folder/*.ply; do
-#     # python fsct/run.py --point-cloud $file --batch_size 5 --odir $data_folder --model ./fsct/model/model.pth
-#     python fsct/run.py --point-cloud $file --batch_size 5 --odir $data_folder
-# done
+# # iterate over all files in the input folder and do sematic segmentation
+echo  "Starting semantic segmentation"
+for file in $data_folder/*.ply; do
+    # python fsct/run.py --point-cloud $file --batch_size 5 --odir $data_folder --model ./fsct/model/model.pth
+    python fsct/run.py --point-cloud $file --batch_size 5 --odir $data_folder --verbose
+done
 
-# # move the output of the first step to the input folder of the second step
-# mkdir -p $data_folder/segmented_point_clouds
+# move the output of the first step to the input folder of the second step
+mkdir -p $data_folder/segmented_point_clouds
 
-# # Check if the file exists and if they exist move them to the new folder
-# if [ -f "$data_folder/*.ply" ]
-# then
-#     mv $data_folder/*.ply $data_folder/segmented_point_clouds
-# fi
+# Check if the file exists and if they exist move them to the new folder
+if [ -f $data_folder/*.segmented.ply ]; then
+    mv $data_folder/*.segmented.ply $data_folder/segmented_point_clouds
+fi
 
-# mv $data_folder/*.segmented.ply $data_folder/segmented_point_clouds
-
-# # do the tiling and tile index generation
-# echo "Tiling and tile index generation"
-# python nibio_preprocessing/tiling.py -i $data_folder/segmented_point_clouds/ -o $data_folder/segmented_point_clouds/tiled
+# do the tiling and tile index generation
+echo "Tiling and tile index generation"
+python nibio_preprocessing/tiling.py -i $data_folder/segmented_point_clouds/ -o $data_folder/segmented_point_clouds/tiled
 
 #  iterate over all the segmented point clouds
 # check if the output folder exists
